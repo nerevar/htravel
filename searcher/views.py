1db7c4fd229f0a68e51549ae55c17050934191e1
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2018-02-26 18:30:35 +0300

    show just list of routes

--- searcher/views.py
+++ searcher/views.py
@@ -4,4 +4,8 @@ from robot.models import Way, Route
 
 def main(request):
     way = Way.objects.get(type='TRAIN', from_city_id='1', to_city_id='3')
-    return render(request, './main.html', {})
+    routes = Route.objects.filter(way=way)
+    return render(request, 'tickets.html', {
+        'way': way,
+        'routes': routes
+    })
