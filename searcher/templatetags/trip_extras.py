9fdd197f5717b71cc33d3a3f01302d52f5ddd830
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2018-04-01 17:35:13 +0300

    ссылка на все билеты по городу и по дате

--- searcher/templatetags/trip_extras.py
+++ searcher/templatetags/trip_extras.py
@@ -10,3 +10,13 @@ def show_trip(trip, type, routes_limit=3):
         'type': type,
         'SHOW_ROUTES_COUNT': ':{}'.format(routes_limit)
     }
+
+
+@register.inclusion_tag('city_link.html')
+def city_link(city):
+    return {'city_from_str': 'moscow', 'city_to': city}
+
+
+@register.inclusion_tag('date_link.html')
+def date_link(date_to):
+    return {'date_to': date_to}
